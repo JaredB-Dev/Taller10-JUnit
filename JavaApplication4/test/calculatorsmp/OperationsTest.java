8ae3dfcc87c8bb68851c258ff8777eb18b7534c5
Selim Beltrán <jarbeltr@espol.edu.ec>
2025-07-30 15:17:04 -0500

    CambioV2
    Selim Beltran

---
+++ JavaApplication4/test/calculatorsmp/OperationsTest.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package calculatorsmp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Selim
+ */
+public class OperationsTest {
+    
+    public OperationsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMakeFormula() {
+        System.out.println("MakeFormula");
+        String expResult = "";
+        String result = Operations.MakeFormula();
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testSolve() {
+        System.out.println("Solve");
+        String formula = "";
+        String expResult = "";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+    
+}
